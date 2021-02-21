687736554c5818f86c37bf9042b85a9fe00f5ac9
DivyuOpenSource <shukladivyanshu745893@gmail.com>
2021-02-21 21:32:19 +0530

    commit for header

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyB8WbrftOSOjRGNM5_n_opGLOX-TezSJmU",
+    authDomain: "app-clone-c0944.firebaseapp.com",
+    projectId: "app-clone-c0944",
+    storageBucket: "app-clone-c0944.appspot.com",
+    messagingSenderId: "266850529454",
+    appId: "1:266850529454:web:0fc30c2094e13d71033d3f",
+    measurementId: "G-EYS4P15QY7"
+  };
