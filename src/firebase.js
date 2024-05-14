5605d999a70735287b3061bc650c4946936bf105
Divyanshu Shukla <shukladivyanshu745893@gmail.com>
2024-05-14 23:14:22 +0530

    Added secret variable's in .env and updated version of react-scripts

--- src/firebase.js
+++ src/firebase.js
@@ -1,20 +1,20 @@
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 
-import firebase from 'firebase';
+import firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB8WbrftOSOjRGNM5_n_opGLOX-TezSJmU",
-    authDomain: "app-clone-c0944.firebaseapp.com",
-    projectId: "app-clone-c0944",
-    storageBucket: "app-clone-c0944.appspot.com",
-    messagingSenderId: "266850529454",
-    appId: "1:266850529454:web:0fc30c2094e13d71033d3f",
-    measurementId: "G-EYS4P15QY7"
-  };
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGEING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
+};
 
 const firebaseApp = firebase.initializeApp(firebaseConfig);
 
 const db = firebaseApp.firestore();
 const auth = firebase.auth();
 
-export {db,auth};
+export { db, auth };
